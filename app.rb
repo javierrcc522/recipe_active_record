e76b6a3b48b30d893b284d13cf068ab0c5d557c5
Javier Jimenez and Keegan Ruebling <javier@>
2017-09-27 15:11:38 -0700

    add ability to create categories from index

--- app.rb
+++ app.rb
@@ -5,11 +5,13 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
   @recipes = Recipe.all()
+  @categories = Category.all()
   erb(:index)
 end
 
 get('/create_recipe') do
   @recipes = Recipe.all()
+  @categories = Category.all()
   erb(:index)
 end
 
@@ -17,6 +19,21 @@ post('/create_recipe') do
   name = params['name']
   rating = params['rating']
   recipe = Recipe.create({:name => name, :rating => rating})
+  @recipes = Recipe.all().sort_by {|recipe| recipe.rating}
+  @categories = Category.all()
+  erb(:index)
+end
+
+get('/create_category') do
+  @categories = Category.all()
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+post('/create_category') do
+  tag = params['tag']
+  category = Category.create({:tag => tag})
+  @categories = Category.all().sort_by {|category| category.tag}
   @recipes = Recipe.all()
   erb(:index)
 end
