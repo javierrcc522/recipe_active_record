14534af9a5dff00d5ef80ce9672af4514c6971a7
Javier Jimenez and Keegan Ruebling <keegan@>
2017-09-27 14:36:34 -0700

    add a range limit and require input

--- app.rb
+++ app.rb
@@ -2,3 +2,21 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+get('/create_recipe') do
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+post('/create_recipe') do
+  name = params['name']
+  rating = params['rating']
+  recipe = Recipe.create({:name => name, :rating => rating})
+  @recipes = Recipe.all()
+  erb(:index)
+end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:categories)
   has_and_belongs_to_many(:ingredients)
+
+  before_save :valid
+end
+
+private
+
+def valid
+  validates_inclusion_of :rating, :in => 1..10
 end
